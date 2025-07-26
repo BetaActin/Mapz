23f4b0db4154ce5d122f6ee12fab5e761e8a7eb4
BetaActin <amit.unicom.breeding@gmail.com>
2025-07-26 13:45:07 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <nav style={{ marginBottom: 24 }}>
-          <Link to="/" style={{ marginRight: 20 }}>Map Creator</Link>
-          <Link to="/viewer">Map Viewer</Link>
+        <nav style={{ marginBottom: 24, padding: '10px 0', borderBottom: '1px solid #ccc' }}>
+          <Link to="/" style={{ marginRight: 20, textDecoration: 'none', color: '#333', fontWeight: 'bold' }}>Map Creator</Link>
+          <Link to="/viewer" style={{ textDecoration: 'none', color: '#333', fontWeight: 'bold' }}>Map Viewer</Link>
         </nav>
         <Routes>
           <Route path="/" element={<MapCreator />} />
